f91524d85bee33a7095b150cbc98844cc5cf12b4
kokosx <agabart11@gmail.com>
2023-03-23 18:39:02 +0100

    Moved exam logic to custom hook

--- hooks/useExam.tsx
+++ hooks/useExam.tsx
@@ -7,15 +7,91 @@ type Params = {
 };
 
 export const useExam = ({ initialAnswers, kitQuestions }: Params) => {
-  const [possibleAnswers, setPossibleAnswers] = useState(initialAnswers);
   const [index, setIndex] = useState(0);
   const [correctAnswerId, setCorrectAnswerId] = useState<string>();
   const [wrongAnswerId, setWrongAnswerId] = useState<string>();
+  const [didEnd, setDidEnd] = useState(false);
 
   const [answeredCorrectly, setAnsweredCorrectly] = useState<string[]>([]);
   const [isLocked, setIsLocked] = useState(false);
 
-  const answer = (id: string) => {};
+  const getPossibleAnswersList = () => {
+    if (index === 0) {
+      return initialAnswers;
+    }
+
+    const possibleAnswers = [{ index: index, id: kitQuestions[index].id }];
+    //There must be at least 2 possible answers;
+    const targetLength = kitQuestions.length > 5 ? 5 : kitQuestions.length;
+    while (possibleAnswers.length < targetLength) {
+      const randomIndex = Math.floor(Math.random() * kitQuestions.length);
+      //Check if this index already exists:
+      let shouldSkip = false;
+      for (let i = 0; i < possibleAnswers.length; i++) {
+        if (randomIndex === possibleAnswers[i].index) {
+          shouldSkip = true;
+        }
+      }
+      if (shouldSkip) {
+        continue;
+      }
+      possibleAnswers.push({
+        index: randomIndex,
+        id: kitQuestions[randomIndex].id,
+      });
+    }
+
+    //Return as an array
+    return possibleAnswers;
+  };
+
+  const [possibleAnswers, setPossibleAnswers] = useState(
+    getPossibleAnswersList()
+  );
+
+  const handleAnswering = (answerIndex: number) => {
+    if (possibleAnswers[answerIndex].id === kitQuestions[index].id) {
+      setCorrectAnswerId(possibleAnswers[answerIndex].id);
+      setAnsweredCorrectly([
+        ...answeredCorrectly,
+        possibleAnswers[answerIndex].id,
+      ]);
+    } else {
+      setWrongAnswerId(possibleAnswers[answerIndex].id);
+      setCorrectAnswerId(kitQuestions[index].id);
+    }
+
+    window.scrollTo({
+      top: 99999,
+
+      behavior: "smooth",
+    });
+
+    setIsLocked(true);
+  };
+
+  const setNextAnswer = () => {
+    setCorrectAnswerId(undefined);
+    setWrongAnswerId(undefined);
+    setIsLocked(false);
+
+    if (index >= kitQuestions.length - 1) {
+      setDidEnd(true);
+    } else {
+      setIndex(index + 1);
+      setPossibleAnswers(getPossibleAnswersList());
+    }
+  };
+
+  const getBgClass = (answerId: string) => {
+    if (answerId === correctAnswerId) {
+      return "bg-success";
+    } else if (answerId === wrongAnswerId) {
+      return "bg-error";
+    } else {
+      return "bg-neutral";
+    }
+  };
 
   return {
     possibleAnswers,
@@ -24,6 +100,10 @@ export const useExam = ({ initialAnswers, kitQuestions }: Params) => {
     correctAnswerId,
     setCorrectAnswerId,
     isLocked,
-    answer,
+    getBgClass,
+    didEnd,
+    answeredCorrectly,
+    setNextAnswer,
+    handleAnswering,
   };
 };
--- pages/kit/[id]/exam.tsx
+++ pages/kit/[id]/exam.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 import Layout from "../../../components/layout";
 import ErrorDialog from "../../../components/styled/ErrorDialog";
+import { useExam } from "../../../hooks/useExam";
 import { prismaClient } from "../../../server/prisma";
 import { isUserLoggedIn } from "../../../services/auth.service";
 
@@ -17,100 +18,26 @@ type Props = {
 };
 
 const Learn = ({ kit, nickname, initialAnswers }: Props) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const {
+    getBgClass,
+    didEnd,
+    answeredCorrectly,
+    index,
+    isLocked,
+    possibleAnswers,
+    setNextAnswer,
+    handleAnswering,
+  } = useExam({ initialAnswers, kitQuestions: kit.questions });
 
-  const [correctAnswer, setCorrectAnswer] = useState<string>();
-  const [wrongAnswer, setWrongAnswer] = useState<string>();
-
-  const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
-
-  const [didEnd, setDidEnd] = useState(false);
-  const [isLocked, setIsLocked] = useState(false);
   const [error, setError] = useState(false);
-
-  const [answers, setAnswers] =
-    useState<{ id: string; index: number }[]>(initialAnswers);
 
   const router = useRouter();
 
-  const getAnswersList = () => {
-    //If its first run, return answers generated on the server;
-    if (currentIndex === 0) {
-      return initialAnswers;
-    }
-
-    const possibleAnswers = [
-      { index: currentIndex, id: kit.questions[currentIndex].id },
-    ];
-    //There must be at least 2 possible answers;
-    const targetLength = kit.questions.length > 5 ? 5 : kit.questions.length;
-    while (possibleAnswers.length < targetLength) {
-      const randomIndex = Math.floor(Math.random() * kit.questions.length);
-      //Check if this index already exists:
-      let shouldSkip = false;
-      for (let i = 0; i < possibleAnswers.length; i++) {
-        if (randomIndex === possibleAnswers[i].index) {
-          shouldSkip = true;
-        }
-      }
-      if (shouldSkip) {
-        continue;
-      }
-      possibleAnswers.push({
-        index: randomIndex,
-        id: kit.questions[randomIndex].id,
-      });
-    }
-
-    //Return as an array
-    return possibleAnswers;
-  };
-
-  const handleClosedAnswer = (answerIndex: number) => {
-    if (answers[answerIndex].id === kit.questions[currentIndex].id) {
-      setCorrectAnswer(answers[answerIndex].id);
-      setCorrectAnswers([...correctAnswers, answers[answerIndex].id]);
-    } else {
-      setWrongAnswer(answers[answerIndex].id);
-      setCorrectAnswer(kit.questions[currentIndex].id);
-    }
-
-    window.scrollTo({
-      top: 99999,
-
-      behavior: "smooth",
-    });
-
-    setIsLocked(true);
-  };
-
-  const setNextAnswer = () => {
-    setCorrectAnswer(undefined);
-    setWrongAnswer(undefined);
-    setIsLocked(false);
-    if (currentIndex + 1 === kit.questions.length) {
-      setDidEnd(true);
-    } else {
-      setCurrentIndex(currentIndex + 1);
-      setAnswers(getAnswersList());
-    }
-  };
-
-  const getBgClass = (answerId: string) => {
-    if (answerId === correctAnswer) {
-      return "bg-success";
-    } else if (answerId === wrongAnswer) {
-      return "bg-error";
-    } else {
-      return "bg-neutral";
-    }
-  };
-
   const renderPossibleClosedAnswers = () => {
-    return answers.map((v, i) => {
+    return possibleAnswers.map((v, i) => {
       return (
         <button
-          onClick={() => (isLocked ? null : handleClosedAnswer(i))}
+          onClick={() => (isLocked ? null : handleAnswering(i))}
           key={v.id}
           className={`flex items-center hover:scale-[1.01] duration-200 ease-in-out justify-center p-2  rounded-md min-h-[200px] text-lg text-center break-all  ${getBgClass(
             v.id
@@ -127,11 +54,11 @@ const Learn = ({ kit, nickname, initialAnswers }: Props) => {
       <>
         <h2 className="text-4xl font-semibold text-secondary">{kit.name}</h2>
         <p className="text-xl">
-          {currentIndex + 1} / {kit.questions.length}
+          {index + 1} / {kit.questions.length}
         </p>
         <div className="flex items-center justify-center p-2  rounded-md min-h-[200px] bg-neutral">
           <p className="text-lg text-center break-all ">
-            {kit.questions[currentIndex].question}
+            {kit.questions[index].question}
           </p>
         </div>
         <hr />
@@ -145,9 +72,7 @@ const Learn = ({ kit, nickname, initialAnswers }: Props) => {
           onClick={setNextAnswer}
           className="btn btn-primary"
         >
-          {currentIndex + 1 === kit.questions.length
-            ? "Zakończ"
-            : "Następne pytanie"}
+          {index + 1 === kit.questions.length ? "Zakończ" : "Następne pytanie"}
         </button>
       </>
     );
@@ -155,8 +80,8 @@ const Learn = ({ kit, nickname, initialAnswers }: Props) => {
 
   const showEndView = () => {
     const isAnswerCorrect = (id: string) => {
-      const val = correctAnswers.filter((v) => v === id).length === 1;
-      console.log(val);
+      const val = answeredCorrectly.filter((v) => v === id).length === 1;
+
       return val;
     };
 
@@ -167,11 +92,13 @@ const Learn = ({ kit, nickname, initialAnswers }: Props) => {
           <div className="flex items-center w-full gap-x-2">
             <progress
               className="progress progress-success bg-neutral"
-              value={(correctAnswers.length / kit.questions.length) * 100}
+              value={(answeredCorrectly.length / kit.questions.length) * 100}
               max="100"
             ></progress>
             <p>
-              {Math.floor((correctAnswers.length / kit.questions.length) * 100)}
+              {Math.floor(
+                (answeredCorrectly.length / kit.questions.length) * 100
+              )}
               %
             </p>
           </div>
